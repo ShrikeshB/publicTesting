6080d400928b5c2ff9a8585acd31a6a77df03dfb
ShrikeshB <shrikeshbandivadekarpro95@gmail.com>
2025-04-08 10:42:17 +0530

    1

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// Load environment variables
+const app = express();
+
+// Middleware
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Your React frontend URL
+    credentials: true, // Allow cookies to be sent
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+app.get("/greeting", (req, res) => {
+  res.status(201).json({ message: "hello sir " });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log("\n--------------------------------------");
+  console.log(`Server running on port ${PORT}`);
+});
